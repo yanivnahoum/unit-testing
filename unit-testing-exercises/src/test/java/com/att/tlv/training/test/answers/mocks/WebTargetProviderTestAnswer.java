57fd8769db00abb9f3bc28cbcfba71cd8b16f334
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2017-11-21 15:02:17 +0200

    [UT] Added another option for testing unhappy flow

--- unit-testing-exercises/src/test/java/com/att/tlv/training/test/answers/mocks/WebTargetProviderTestAnswer.java
+++ unit-testing-exercises/src/test/java/com/att/tlv/training/test/answers/mocks/WebTargetProviderTestAnswer.java
@@ -1,12 +1,14 @@
 package com.att.tlv.training.test.answers.mocks;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.junit.Before;
@@ -62,5 +64,10 @@ public class WebTargetProviderTestAnswer {
         when(client.target(url)).thenThrow(ex);
         
         assertThatThrownBy(() -> webTargetProvider.get(url)).isSameAs(ex);
+        
+        // Another option (which doesn't use stubbing for exceptions)
+        Client newClient = ClientBuilder.newClient();
+        WebTargetProvider provider = new WebTargetProvider(newClient);
+        assertThatNullPointerException().isThrownBy(() -> provider.get(url));
     }
 }
