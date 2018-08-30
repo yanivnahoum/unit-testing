d5af7f0a3f2d7b71376f7cc84fb7977ec7048822
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2018-08-30 12:33:58 +0300

    [validate-setup] Added a simple test to make sure project is configured correctly

---
+++ unit-testing-exercises/src/test/java/com/att/tlv/training/test/HelloUnitTesting.java
@@ -0,0 +1,16 @@
+package com.att.tlv.training.test;
+
+import org.junit.Test;
+
+/**
+ * Run this test class and make sure you see a message in the console.
+ * Intellij: Ctrl+Shift+F10 (Win) / Ctrl+Shift+R,Ctrl+Shift+D (Mac)
+ * Eclipse: Alt+Shift+X,T
+ */
+public class HelloUnitTesting {
+
+    @Test
+    public void makeSureProjectIsSetUpCorrectly() {
+        System.out.println("You're ready to go!");
+    }
+}
