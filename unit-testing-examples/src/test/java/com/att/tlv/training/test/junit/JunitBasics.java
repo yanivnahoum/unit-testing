58af9babbdbed530916d7b3799a63fef2fa64f19
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2019-06-26 14:23:21 +0300

    [junit] Added @DisplayName and @Disabled variations to basic example

--- unit-testing-examples/src/test/java/com/att/tlv/training/test/junit/JunitBasics.java
+++ unit-testing-examples/src/test/java/com/att/tlv/training/test/junit/JunitBasics.java
@@ -4,32 +4,48 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+
+import static org.junit.jupiter.api.condition.JRE.JAVA_8;
 
 // No need for a public class anymore!
 class JunitBasics {
     
-    // MUST be a public method!
+    // MUST not be a private method!
     @Test
     void test1() {
         System.out.println("Running test1!");
     }
     
     @Test
+    @DisplayName("Test #2")
     void test2() {
         System.out.println("Running test2!");
+    }
+
+    @Test
+    @DisplayName("Test #3")
+    @Disabled
+    @DisabledOnJre(JAVA_8)
+    @DisabledIfSystemProperty(named = "env-type", matches = "ci")
+    void test3() {
+        System.out.println("Running test3!");
     }
     
     
     // Set up and tear down methods: all methods can be package private
     
-    // MUST be a static method!
+    // MUST be a static non-private method!
     @BeforeAll
     static void setUpClass() {
         System.out.println("Running set up before class...");
     }
     
-    // MUST be a static method!
+    // MUST be a static non-private method!
     @AfterAll
     static void tearDownClass() {
         System.out.println("Running tear down after class...");
