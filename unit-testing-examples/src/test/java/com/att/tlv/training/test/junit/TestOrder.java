6b67cb8e23267bec4e90ed9639a12ae73cf60fd3
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2019-07-03 14:37:19 +0300

    [junit] Some minor changes + added test order (@Order) example

---
+++ unit-testing-examples/src/test/java/com/att/tlv/training/test/junit/TestOrder.java
@@ -0,0 +1,26 @@
+package com.att.tlv.training.test.junit;
+
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(OrderAnnotation.class)
+class TestOrder {
+
+    @Order(1)
+    @Test
+    void first() {
+    }
+
+    @Order(3)
+    @Test
+    void third() {
+    }
+
+    @Order(2)
+    @Test
+    void test2() {
+    }
+}
