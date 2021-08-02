3963df9e32bdd86bcba2f7d4ee30d943307fb782
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2021-08-02 20:24:01 +0300

    [build] Upgraded dependencies, gradle and maven

--- unit-testing-exercises/src/test/java/com/att/tlv/training/test/HelloUnitTesting.java
+++ unit-testing-exercises/src/test/java/com/att/tlv/training/test/HelloUnitTesting.java
@@ -3,8 +3,8 @@ package com.att.tlv.training.test;
 import org.junit.jupiter.api.Test;
 
 /**
- * Run this test class and make sure you see a message in the console.
- * Intellij: Ctrl+Shift+F10 (Win) / Ctrl+Shift+R,Ctrl+Shift+D (Mac)
+ * Run this test class and make sure you see a message in the console. <br/>
+ * Intellij: Ctrl+Shift+F10 (Win) / Cmd+Shift+R (Mac) <br/>
  * Eclipse: Alt+Shift+X,T
  */
 class HelloUnitTesting {
