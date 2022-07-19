23aeaed70f9201dfa653680e6ed4cb5897f06d1e
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2022-07-19 14:12:00 +0300

    Upgraded to maven wrapper (3.8.6), gradle 7.5, latest mockito, assertj, jersey, junit-jupiter, guava, maven-compiler-plugin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    const val guava = "30.1.1-jre"
-    const val jersey = "3.0.2"
-    const val junitJupiter = "5.7.2"
-    const val mockito = "3.11.2"
-    const val assertj = "3.20.2"
+    const val guava = "31.1-jre"
+    const val jersey = "3.0.5"
+    const val junitJupiter = "5.8.2"
+    const val mockito = "4.6.1"
+    const val assertj = "3.23.1"
 }
