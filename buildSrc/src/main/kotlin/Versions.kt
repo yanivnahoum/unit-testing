ec1053b8ae7db2838bfdcb0a41b6d45dade90159
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2022-10-18 16:22:17 +0300

    Bumped junit-jupiter to 5.9.1 and Mockito to 4.8.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val guava = "31.1-jre"
     const val jersey = "3.0.5"
-    const val junitJupiter = "5.8.2"
-    const val mockito = "4.6.1"
+    const val junitJupiter = "5.9.1"
+    const val mockito = "4.8.1"
     const val assertj = "3.23.1"
 }
