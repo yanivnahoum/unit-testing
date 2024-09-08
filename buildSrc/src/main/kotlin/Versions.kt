2d78dce872cd5010c8537adf331759b3e67116c6
Yaniv Nahoum <yaniv.nahoum@intl.att.com>
2024-09-08 19:17:17 +0300

    Upgraded maven, gradle and dependencies. Added @FieldSource

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    const val guava = "33.2.1-jre"
+    const val guava = "33.3.0-jre"
     const val jersey = "3.1.8"
-    const val junitJupiter = "5.10.3"
-    const val mockito = "5.12.0"
+    const val junitJupiter = "5.11.0"
+    const val mockito = "5.13.0"
     const val assertj = "3.26.3"
 }
--- unit-testing-examples/src/test/java/com/att/tlv/training/test/junit/JunitParameterizedTests.java
+++ unit-testing-examples/src/test/java/com/att/tlv/training/test/junit/JunitParameterizedTests.java
@@ -8,13 +8,16 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.FieldSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.DayOfWeek;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Named.named;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.junit.jupiter.params.provider.EnumSource.Mode.MATCH_ALL;
 
@@ -74,6 +77,7 @@ class JunitParameterizedTests {
 
     @ParameterizedTest
     @MethodSource
+    @SuppressWarnings("java:S4144")
     void maxShouldReturnGreaterNumber(int x, int y, int expected) {
         var calculator = new Calculator();
         int actual = calculator.max(x, y);
@@ -90,6 +94,44 @@ class JunitParameterizedTests {
         var calculator = new Calculator();
         int actual = calculator.max(x, y);
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Nested
+    class FieldSources {
+        private Calculator calculator;
+
+        @BeforeEach
+        void setUp() {
+            calculator = new Calculator();
+        }
+
+        @ParameterizedTest
+        @FieldSource("absArguments")
+        void givenValue_theAbsShouldReturnAbsolute(int input, int expected) {
+            int actual = calculator.abs(input);
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        static Supplier<Stream<Arguments>> absArguments = () -> Stream.of(
+                arguments(-1, 1),
+                arguments(0, 0),
+                arguments(1, 1)
+        );
+
+        @ParameterizedTest
+        @FieldSource
+        void absShouldReturnAbsolute(AbsArguments args) {
+            int actual = calculator.abs(args.input());
+            assertThat(actual).isEqualTo(args.expected());
+        }
+
+        record AbsArguments(int input, int expected) {}
+
+        static Supplier<Stream<Arguments>> absShouldReturnAbsolute = () -> Stream.of(
+                arguments(named("Negative", new AbsArguments(-1, 1))),
+                arguments(named("Zero", new AbsArguments(0, 0))),
+                arguments(named("Positive", new AbsArguments(1, 1)))
+        );
     }
 
     @Nested
