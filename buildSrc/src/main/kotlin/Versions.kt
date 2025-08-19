6ed500cfbe7a2cea88ec8a7d95dfb4d9906ba753
Yaniv Nahoum <yaniv.nahoum@intl.att.com>
2025-08-19 18:58:37 +0300

    Bump Mockito version to 5.19.0 in pom.xml and Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val guava = "33.4.8-jre"
     const val jersey = "3.1.11"
     const val junitJupiter = "5.13.4"
-    const val mockito = "5.18.0"
+    const val mockito = "5.19.0"
     const val assertj = "3.27.4"
 }
